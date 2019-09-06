2220ce2a6a1394681fba9427238634952f414e0b
Richard <richard@Richards-MacBook-Pro.local>
2019-09-06 16:52:02 -0400

    Fix example for ios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { CircuitService } from './services/circuit.service';
 
 declare var window: any;
 declare var wkWebView: any;
@@ -16,7 +17,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private circuit: CircuitService
   ) {
     this.initializeApp();
   }
@@ -25,6 +27,10 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      if (this.platform.is('ios')) {
+        this.circuit.initWebRTC();
+      }
     });
   }
 }
